8ce69bd992c08bb7fe25cc295d0642b187012bb9
JaspreetBagga <78042377+JaspreetBagga@users.noreply.github.com>
2021-09-03 00:04:07 +0530

    Add files via upload

---
+++ Escape1.java
@@ -0,0 +1,11 @@
+/* Escape Sequences uses */
+/*import javax.swing.*; 	 */
+public class Escape1
+{
+	public static void main(String args[])
+	{
+		System.out.print("Clock");
+		System.out.print("\r");
+		System.out.print("Triumph");	
+	}
+}
